fe479fa32156fcc40cc740ae130d49d7f2346fd1
Bagus Setiawan <bagustrapp24@gmail.com>
2026-09-10 00:20:14 +0700

    Connect Vercel to local Flask port 5000 via Ngrok

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,19 +12,14 @@
 //   },
 // };
 
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.CAPACITOR_BUILD === "true" ? "export" : undefined,
   async rewrites() {
-    if (process.env.CAPACITOR_BUILD === "true" || process.env.NODE_ENV === "production") {
-      return [];
-    }
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: "https://liquefy-crinkle-criteria.ngrok-free.dev/api/:path*",
       },
     ];
   },
